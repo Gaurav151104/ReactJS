229062ea7a2a5c9f096e19257b91f6c01de9828f
Gaurav151104 <gauravnalavade6042@gmail.com>
2026-08-22 19:35:52 +0530

    Learning the Reach Components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+
+
+
+const App = () => {
+ 
+   
+  return (
+    <div>
+      <div className='card'>
+        <h1>Garry</h1>
+      </div>
+          <Card/>
+          <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav">
+            <h1>Hi I am from the navbar</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
