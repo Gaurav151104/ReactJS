3418e4603c4e2a5f77fb8c26f692fdf5115cb09c
Gaurav151104 <gauravnalavade6042@gmail.com>
2026-08-29 12:07:51 +0530

    Styling with the CSS

---
+++ 05_CSS/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 
+const Header = () => {
+  return (
+    <div className='header'>
+      <h3>Garry ...King in the North</h3>
+      <button className='btn'>Login</button>
+    </div>
+  )
+}
+
+export default Header
